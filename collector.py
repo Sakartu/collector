f70935773b88b2c6e26d0d302dd54ef487fc6630
Peter Wagenaar <sakartu@gmail.com>
2014-04-22 17:55:17 +0200

    Reformat file

--- collector.py
+++ collector.py
@@ -1,14 +1,4 @@
 #!/usr/bin/env python
-
-#***********************************<Config>***********************************
-# READER_DIR specifies the path to the root directory of the Sony Reader. This
-# can either be a linux path or a Windows drive. This configuration parameter
-# is *not* optional, it must be configured for collector to work properly.
-
-#READER_DIR = 'e:/'
-READER_DIR = '/media/READER/'
-#**********************************</Config>***********************************
-
 try:
     import os
     import sys
@@ -22,6 +12,16 @@ except ImportError:
     print u'- shutil'
     print u'- sqlite3'
     print u'- subprocess'
+    raise
+
+#***********************************<Config>***********************************
+# READER_DIR specifies the path to the root directory of the Sony Reader. This
+# can either be a linux path or a Windows drive. This configuration parameter
+# is *not* optional, it must be configured for collector to work properly.
+
+#READER_DIR = 'e:/'
+READER_DIR = '/Volumes/READER/'
+#**********************************</Config>***********************************
 
 db_path = os.path.join(READER_DIR, 'Sony_Reader/database/books.db')
 db_backup = db_path + '.bak'
@@ -50,8 +50,7 @@ try:
         for root, files in collectable_files.items():
             ids = []
             for f in files:
-                c.execute('''SELECT _id FROM books WHERE file_name = (?) OR
-                        file_path LIKE (?)''', (f, "%" + f + "%",))
+                c.execute('''SELECT _id FROM books WHERE file_name = (?) OR file_path LIKE (?)''', (f, "%" + f + "%",))
                 results = c.fetchone()
                 if not results:
                     print u'Could not get ID for ' + f + '!'
@@ -65,29 +64,22 @@ try:
             if 'y' not in raw_input('Process "' + root + '"? ').lower():
                 continue
 
-            c.execute('''SELECT _id FROM collection WHERE title = (?)''',
-                    (root,))
+            c.execute('''SELECT _id FROM collection WHERE title = (?)''', (root,))
             results = c.fetchone()
             if not results:
-                c.execute('''INSERT OR IGNORE INTO collection (title,
-                        kana_title, source_id, uuid) VALUES (?, ?, ?, ?)''',
-                        (root, '', 0, ''))
-                c.execute('''SELECT _id FROM collection WHERE title = (?)''',
-                        (root,))
+                c.execute('''INSERT OR IGNORE INTO collection (title, kana_title, source_id, uuid) VALUES (?, ?, ?, ?)''', (root, '', 0, ''))
+                c.execute('''SELECT _id FROM collection WHERE title = (?)''', (root,))
                 results = c.fetchone()
                 if not results:
                     print u'Could not get collection ID for ' + root + '!'
                     continue
             coll_id = results[0]
             for index, f_id in enumerate(collectable_files[root]):
-                c.execute('''SELECT * FROM collections WHERE collection_id =
-                        (?) AND content_id = (?)''', (coll_id, f_id))
+                c.execute('''SELECT * FROM collections WHERE collection_id = (?) AND content_id = (?)''', (coll_id, f_id))
                 if c.fetchone():
                     print u'Book already in collection, moving on!'
                     continue
-                c.execute('''INSERT INTO collections (collection_id,
-                        content_id, added_order) VALUES (?, ?, ?)''',
-                        (coll_id, f_id, index))
+                c.execute('''INSERT INTO collections (collection_id, content_id, added_order) VALUES (?, ?, ?)''', (coll_id, f_id, index))
                 print u'Added book to collection "' + root + '"!'
 
     if os.name == 'posix':
